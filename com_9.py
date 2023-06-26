2fad632a7d6d7be065d905a5ef5f5d09be97e7de
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-06-26 10:06:47 +0530

    Create com_9.py

---
+++ com_9.py
@@ -0,0 +1,17 @@
+num=int(input("Enter the number:\n"))
+if num==1:
+    print("Sunday")
+elif num==2:
+    print("Monday")
+elif num==3:
+    print("Tuesday")
+elif num==4:
+    print("Wednesday")
+elif num==5:
+    print("Thrusday")
+elif num==6:
+    print("Friday")
+elif num==7:
+    print("Saturday")
+else:
+    print("Your input is invalid")
