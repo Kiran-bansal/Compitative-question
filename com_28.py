d40b84b7d9a6289c904e73472273b877e1768c0a
Kiran Bansal <136809550+Kiran-bansal@users.noreply.github.com>
2023-06-27 23:55:43 +0530

    Create com_28.py

---
+++ com_28.py
@@ -0,0 +1,9 @@
+row_num = int(input("Enter number of rows: "))
+col_num = int(input("Enter number of columns: "))
+multi_list = [[0 for col in range(col_num)] for row in range(row_num)]
+
+for row in range(row_num):
+    for col in range(col_num):
+        multi_list[row][col]= row*col
+
+print(multi_list)
